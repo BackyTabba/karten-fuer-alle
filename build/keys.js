e970e6ae7666191a459e659ea7234d7bbe3f614f
BackyTabba <48299231+BackyTabba@users.noreply.github.com>
2022-12-04 18:19:10 +0100

    Current Test of Github workflow

--- build/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoHost: process.env.MONGO_HOST,
-  mongoPort: process.env.MONGO_PORT,
-  mongoAdmin: process.env.MONGO_INITDB_ROOT_USERNAME,  
-  mongoAdminPW: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  params:{"ToolName":"Template","content":{"start":"true","Polygon":"true","Marker":"true","Rectangle":"true","Circle":"true","Polyline":"true","end":"true","open":"true","save":"true","end2":"true","OpenStreetMap":"true","CartoDb.Positron":"true","CartoDB.DarkMatter":"true","CartoDB.Voyager":"true","OpenStreetMap.Mapnik":"true","OpenTopoMap":"true","Stadia.AlidadeSmoothDark":"true","Stadia.OSMBright":"true","Esri.WorldImagery":"true"},"schema":"Asave","DataTypeInput":"Attribute1:String,\r\nAttributBeispiel:Number,\r\nAttribute1245:Boolean "}
-};
